079797dbc93345a3ef2a4d1f4316af8f779fbf49
onemmanuel4christ <onemmanuel4christ@gmail.com>
2023-11-28 14:56:08 -0800

    if else task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 
 for alpha_letters in range(ord('a'), ord('z')+1):
     if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:c}".format(alpha_letters), end="")
+        continue
+            print("{:c}".format(alpha_letters), end="")
 
